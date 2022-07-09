658e2f5674e22081640011079aa53d190396f570
Dean Taylor <deanrtaylor@hotmail.com>
2022-07-09 19:57:25 +0700

    updated buttons, rainbow, eraser, slider, change to black, drop shadow and title

--- script.js
+++ script.js
@@ -1,11 +1,78 @@
 const grid = document.querySelector("#grid");
 const reset = document.querySelector("#reset");
 const rainbow = document.querySelector("#rainbow");
-const tenGrid = document.querySelector("#tenGrid");
-const sixteenGrid = document.querySelector("#sixteenGrid");
-const sixfourGrid = document.querySelector("#sixfourGrid");
+const eraser = document.querySelector("#eraser");
+const pen = document.querySelector("#pen");
+
 let currentgridSize = 257;
+
 let bgColor = 'black'
+
+
+const sizeValue = document.getElementById('sizeValue');
+const sizeSlider = document.getElementById('sizeSlider');
+const DEFAULT_COLOR = '#333333';
+const DEFAULT_SIZE = 16;
+
+let currentColor = DEFAULT_COLOR;
+//let currentMode = DEFAULT_MODE;
+let currentSize = DEFAULT_SIZE;
+
+reset.onclick = () => reloadGrid()
+sizeSlider.onmousemove = (e) => updateSizeValue(e.target.value)
+sizeSlider.onchange = (e) => changeSize(e.target.value)
+
+
+function changeSize(value) {
+    setCurrentSize(value)
+    updateSizeValue(value)
+    reloadGrid()
+  }
+
+  function setCurrentSize(newSize) {
+    currentSize = newSize
+  }
+
+function updateSizeValue(value) {
+    sizeValue.innerHTML = `${value}`
+  }
+
+function clearGrid() {
+    grid.innerHTML = ''
+  }
+
+  function reloadGrid() {
+    clearGrid()
+    setupGrid(currentSize)
+  }
+
+function setupGrid(size) {
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
+
+    for (let i = 0; i < size * size; i++) {
+        const gridElement = document.createElement('div')
+        gridElement.classList.add('grid-element')
+        gridElement.setAttribute(`id`,`${i}`);
+       // gridElement.addEventListener('mouseover', changeColor)
+       // gridElement.addEventListener('mousedown', changeColor)
+        grid.appendChild(gridElement)
+    }
+      
+    };
+
+    
+    
+    window.onload = () => {
+        setupGrid(DEFAULT_SIZE)
+        
+      }
+
+
+
+
+
+
 
 
 
@@ -16,21 +83,8 @@ let b = randomBetween(0, 255);
 let rgb = `rgb(${r},${g},${b})`; // Collect all to a css color string
 
 
-window.addEventListener('load', () => {
 
 
-    grid.style.gridTemplateColumns = "repeat(16, 1fr)";
-    grid.style.gridTemplateRows= "repeat(16, 1fr)";
-
-    for(let i = 1; i < 257; i++){
-
-        
-
-        grid.innerHTML += `<div class= grid-element id=${i} ;></div>`;}
-
-    
-
-});
 
 grid.addEventListener('mouseover', event => {
 
@@ -41,13 +95,18 @@ grid.addEventListener('mouseover', event => {
        
         target.style.backgroundColor =  'black';
     }
+
+    else if (bgColor === 'white') { 
+        target.style.backgroundColor =  'white';
+    
+    }
     
     else { 
         let r = randomBetween(0, 255);
         let g = randomBetween(0, 255);
         let b = randomBetween(0, 255);
         let rgb = `rgb(${r},${g},${b})`; // Collect all to a css color string      
-        console.log(rgb);
+        //console.log(rgb);
         target.style.backgroundColor = rgb;
     
     }
@@ -59,8 +118,9 @@ grid.addEventListener('mouseover', event => {
 reset.addEventListener('click', () => {
 
     bgColor = 'black';
+    console.log(currentSize);
 
-    for(let i = 1; i < (currentgridSize + 1); i++){
+    for(let i = 1; i < (currentSize * currentSize); i++){
 
         let currentBox = document.getElementById(`${i}`);
 
@@ -80,157 +140,18 @@ rainbow.addEventListener('click', () => {
 });
 
 
+eraser.addEventListener('click', () => {  
 
-tenGrid.addEventListener('click', () => {  
-
-    grid.innerHTML = "";
-    grid.style.gridTemplateColumns = "repeat(10, 1fr)";
-    grid.style.gridTemplateRows= "repeat(10, 1fr)";
-
-    for(let i = 1; i < 101; i++){
-
-        
-
-        grid.innerHTML += `<div class= grid-element id=${i} ;></div>`;}
-
-        currentgridSize = 101;
-
-    
-    
-
-});
-
-sixteenGrid.addEventListener('click', () => {  
-
-    grid.innerHTML = "";
-    grid.style.gridTemplateColumns = "repeat(16, 1fr)";
-    grid.style.gridTemplateRows= "repeat(16, 1fr)";
-
-    for(let i = 1; i < 257; i++){
-
-        
-
-        grid.innerHTML += `<div class= grid-element id=${i} ;></div>`;}
-
-        currentgridSize = 257;
-
-    
-    
-
-});
-
-sixfourGrid.addEventListener('click', () => {  
-
-    grid.innerHTML = "";
-    grid.style.gridTemplateColumns = "repeat(64, 1fr)";
-    grid.style.gridTemplateRows= "repeat(64, 1fr)";
-
-    for(let i = 1; i < 4097; i++){
-
-        
-
-        const gridElement = document.createElement('div')
-        gridElement.classList.add('grid-element')
-        //gridElement.setAttribute(id,(${i}));
-        grid.appendChild(gridElement)
-
-}
-
-        currentgridSize = 4097;
-
-    
-    
+    bgColor = 'white';
 
 });
 
 
+pen.addEventListener('click', () => {  
 
-
-
-
-
-
-
-
-/*const container = document.querySelector("#container");
-const box = document.querySelectorAll("#container");
-const reset = document.querySelector("#reset");
-const rainbow = document.querySelector("#rainbow");
-let bgColor = 'black'
-
-
-
-const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
-let r = randomBetween(0, 255);
-let g = randomBetween(0, 255);
-let b = randomBetween(0, 255);
-let rgb = `rgb(${r},${g},${b})`; // Collect all to a css color string
-
-
-
-
-
-
-
-
-
-
-container.addEventListener('mouseover', event => {
-    let target = event.target; 
-
-    if (bgColor === 'black') {
-   
-       
-    target.style.backgroundColor =  bgColor;
-}
-
-else { 
-    let r = randomBetween(0, 255);
-    let g = randomBetween(0, 255);
-    let b = randomBetween(0, 255);
-    let rgb = `rgb(${r},${g},${b})`; // Collect all to a css color string      
-    console.log(rgb);
-    target.style.backgroundColor = rgb;
-
-}
+    bgColor = 'black';
 
 });
-
-reset.addEventListener('click', () => {
-
-    for(let i = 1; i < 257; i++){
-
-        let currentBox = document.getElementById(`${i}`);
-
-        currentBox.style.backgroundColor =  `white`;
-
-}
-
-bgColor = 'black';
-
-
-});
-
-rainbow.addEventListener('click', () => {  
-
-        bgColor = rgb;
-
-});
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
 
 
 
